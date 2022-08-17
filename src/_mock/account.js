076366a2b203cf6a726491f87fc2f33daa9c0aa2
Parthiban <Talktoparthi@gmail.com>
2022-08-17 16:36:49 +0530

    Added my DNK  project

---
+++ src/_mock/account.js
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'General Manager, CEPT',
+  email: 'demo@indiapost.gov.in',
+  photoURL: '/static/mock-images/avatars/avatar_default.jpg',
+};
+
+export default account;
